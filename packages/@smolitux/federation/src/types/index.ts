7341010acc605644c2b59610731938886e3bf24f
sam <sschimmelpfennig@proton.me>
2025-03-27 13:47:50 +0100

    Update index.ts

--- packages/@smolitux/federation/src/types/index.ts
+++ packages/@smolitux/federation/src/types/index.ts
@@ -1,7 +1,2 @@
-// packages/@smolitux/federation/src/types/index.ts
-// Update this file to export from the types.ts file instead
-
 // Export types from the dedicated types file
 export * from './types';
-
-// No component imports or interface definitions here
