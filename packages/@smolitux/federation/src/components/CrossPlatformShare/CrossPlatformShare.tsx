c83be9ec58464577f52e63725a197916649450bf
sam <sschimmelpfennig@proton.me>
2025-03-27 14:09:59 +0100

    Update CrossPlatformShare.tsx

--- packages/@smolitux/federation/src/components/CrossPlatformShare/CrossPlatformShare.tsx
+++ packages/@smolitux/federation/src/components/CrossPlatformShare/CrossPlatformShare.tsx
@@ -677,14 +677,31 @@ export const CrossPlatformShare: React.FC<CrossPlatformShareProps> = ({
                             <svg className="w-5 h-5 ml-auto text-red-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                               <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.
                         </div>
-                      )}
-                    </div>
-                  )}
-                </div>
-              </div>
-            </>
-          )}
-        </div>
-      </Card>
-    );
-  };
+                     )}
+                   </div>
+                 </div>
+               ))}
+             </div>
+           </div>
+           
+           {/* Aktionen */}
+           <div className="flex justify-end space-x-2">
+             <Button
+               variant="outline"
+               onClick={handleClose}
+             >
+               Abbrechen
+             </Button>
+             
+             <Button
+               variant="primary"
+               onClick={handleShare}
+               disabled={isSharing || selectedPlatforms.length === 0 || 
+                 selectedPlatforms.some(platformId => !checkTextLength(platformId) || !checkContentType(platformId))}
+             >
+               {isSharing ? 'Wird geteilt...' : 'Teilen'}
+             </Button>
+           </div>
+         </Card>
+       );
+     };
