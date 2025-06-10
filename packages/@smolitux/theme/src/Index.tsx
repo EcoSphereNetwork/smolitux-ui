46687e8dc48a64dca0e60c043b1c78a4f041c119
sam <sschimmelpfennig@proton.me>
2025-06-11 01:21:38 +0200

    docs: restore status and coverage reports

--- packages/@smolitux/theme/src/Index.tsx
+++ packages/@smolitux/theme/src/Index.tsx
@@ -1,6 +1,5 @@
 // Export all theme related components and utilities
 export * from './types';
-export * from './Default';
 export { defaultTheme } from './Default';
 export * from './themeProvider';
 export * from './themeUtils';
