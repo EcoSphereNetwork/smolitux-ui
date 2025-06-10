6f4e7ab8d4c2ba1c44a44e0fb547d98e3fb693c2
sam <sschimmelpfennig@proton.me>
2025-06-11 00:00:59 +0200

    fix(testing): adjust jest config

--- packages/@smolitux/theme/src/Index.tsx
+++ packages/@smolitux/theme/src/Index.tsx
@@ -1,6 +1,6 @@
 // Export all theme related components and utilities
 export * from './types';
-export * from './defaultTheme';
+export * from './Default';
 export * from './themeProvider';
 export * from './themeUtils';
 
